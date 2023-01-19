82ee3ce5b8f03de337e5ec8b62bf237f418f71ef
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-18 22:57:58 -0300

    Basic setup

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.listen(3000)
+app.get("/", (req, res, next) => {
+  res.render();
+});
+
+app.get("/users", (req, res, next) => {
+  res.render();
+});
+
+app.post('/add-user', (req, res, next) => {
+    res.redirect('/users');
+});
+
+app.listen(3000);
