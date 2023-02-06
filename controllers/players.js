3ae72f49af733257d27324c02354dc810cffad90
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-02-06 18:53:33 -0300

    wip: Detail page

--- controllers/players.js
+++ controllers/players.js
@@ -1,29 +1,29 @@
-const Player = require('../models/player');
+const Player = require("../models/player");
 
 exports.getPlayers = (req, res, next) => {
-    const players = Player.fetchAll();
-    res.render('index', {
-        players: players, 
-        pageTitle: "Players",
-        path: "/players",
-        hasPlayers: players.length > 0
-    })
-}
+  const players = Player.fetchAll();
+  res.render("index", {
+    players: players,
+    pageTitle: "Players",
+    path: "/players",
+    hasPlayers: players.length > 0,
+  });
+};
 
 exports.getPlayerDetail = (req, res, next) => {
-    const playerId = req.params.playerId;
-    console.log(playerId)
-    res.redirect('/')
-}
+  const playerId = req.params.playerId;
+  const playerSelected = Player.findPlayerById(playerId);
+  res.redirect("/");
+};
 
 exports.postAddPlayer = (req, res, next) => {
-    const playerName = req.body.playerName;
-    const imageUrl = req.body.urlImage;
-    const player = new Player(playerName, imageUrl);
-    player.save();
-    res.redirect('/');
-}
+  const playerName = req.body.playerName;
+  const imageUrl = req.body.urlImage;
+  const player = new Player(playerName, imageUrl);
+  player.save();
+  res.redirect("/");
+};
 
 exports.getAddPlayers = (req, res, next) => {
-    res.render('add-players', { pageTitle: 'Add Player', path: '/add-player'})
-}
+  res.render("add-players", { pageTitle: "Add Player", path: "/add-player" });
+};
